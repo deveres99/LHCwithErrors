612f2a0f179c7a5ca69bc7935e5c56c06fdc2819
Frederik F. Van der Veken <frederik@cern.ch>
2025-07-06 12:39:39 +0200

    Clean up

--- 000_build_clean_lattice.py
+++ 000_build_clean_lattice.py
@@ -8,22 +8,27 @@ start = time.time()
 from knob_tools import set_cavity_frequency, add_phase_knob, add_mo_knob, add_tuning_knobs, check_knobs, \
                        add_correction_term_to_dipole_correctors, set_correctors
 from slice_tools import slice_env
-from tuning_tools import inj_settings_clean, tune_line
+from tuning_tools import tune_line
 
 
 # Paths
-acc_models = Path("/eos/project-c/collimation-team/machine_configurations/acc-models/lhc/2025")
-optics = Path("/afs/cern.ch/eng/lhc/optics/runIII/RunIII_dev")
-scenarios = Path("/eos/project-c/collimation-team/machine_configurations/LHC_run3/2025/scenarios")
-
+path_acc_models = Path("/eos/project-c/collimation-team/machine_configurations/acc-models/lhc/2025")
+path_optics = Path("/afs/cern.ch/eng/lhc/optics/runIII/RunIII_dev")
+path_scenarios = Path("/eos/project-c/collimation-team/machine_configurations/LHC_run3/2025/scenarios")
+outfile = Path("lattices/injection_clean.json")
 
 # Load the configuration
-config = yaml.load(scenarios / 'injection.yaml')
+config = yaml.load(path_scenarios / 'injection.yaml')
+settings_clean = {'qx': 62.28,  'qy': 60.31,  'dqx': 0,  'dqy': 0,  'i_mo': 0,   'c_minus': 0}
+# settings_clean = {'qx': 62.31,  'qy': 60.32,  'dqx': 0,  'dqy': 0,  'i_mo': 0,   'c_minus': 0}  # After tune-change
+
+
+# =================================================================================================
 
 
 # Load the model
-env = xt.load_madx_lattice(acc_models / "lhc.seq", reverse_lines=['lhcb2'])
-env.vars.load_madx(optics / config['optics'])
+env = xt.load_madx_lattice(path_acc_models / "lhc.seq", reverse_lines=['lhcb2'])
+env.vars.load_madx(path_optics / config['optics'])
 env.vars.default_to_zero = False
 for line in env.lines.values():
     line.particle_ref = xt.Particles.reference_from_pdg_id('proton', p0c=config['knob_settings']['nrj']*1e9)
@@ -40,7 +45,7 @@ set_correctors(env)
 check_knobs(env, config)
 
 
-# Apply the knob settings
+# Apply the knob settings for this scneario
 for knob, val in config['knob_settings'].items():
     env.vars[knob] = val
 
@@ -51,11 +56,9 @@ for knob, val in config['knob_settings'].items():
 
 # Tuning
 for line in env.lines.values():
-    tune_line(line, **inj_settings_clean)
+    tune_line(line, **settings_clean)
 
 
 # Save the environment
-env.to_json('lattices/injection_clean.json')
-
-
+env.to_json(outfile)
 print(f"Building took {time.time() - start:.2f} seconds")
--- 001_add_aperture.py
+++ 001_add_aperture.py
@@ -1,4 +1,20 @@
+import xtrack as xt
+from pathlib import Path
+import time
+start = time.time()
 
+
+# Paths
+path_acc_models = Path("/eos/project-c/collimation-team/machine_configurations/acc-models/lhc/2025")
+path_layout = Path("/eos/project-c/collimation-team/machine_configurations/layoutdb")
+infile = Path("lattices/injection_clean.json")
+outfile = Path("lattices/injection_with_apertures.json")
+
+
+# =================================================================================================
+
+# Load the environment and the configuration
+env = xt.Environment.from_json(infile)
 
 # TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
 # TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
@@ -7,12 +23,10 @@
 
 print("TODO TODO: aperture not yet implemented")
 
-# Load the environment and the configuration
-env = xt.Environment.from_json('lattices/injection_clean.json')
-
 # Step one: add all apertures from elements and from LayoutDB
 # Step two: misalign aperture and elements based on the mech_sep (is important in the doglegs, both for
 #           aperture and feeddown effects from errors)
 
 # Save the environment
-env.to_json('lattices/injection_with_apertures.json')
+env.to_json(outfile)
+print(f"Adding apertures took {time.time() - start:.2f} seconds")
